5282d58674e990de1fcd0087fa389617b1b68e95
luyunfeng <603707288@qq.com>
2019-12-24 11:15:50 +0800

    'fix'

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -51,7 +51,7 @@ class CorsMiddleware implements MiddlewareInterface
     {
         return $response
             ->withHeader('Access-Control-Allow-Origin', '*')
-            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Headers', '*')
             ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     }
 }
