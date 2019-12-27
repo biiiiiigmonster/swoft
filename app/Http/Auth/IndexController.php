6e5f2755b893f0ec0c58ea8f2dd2fde6ed70e6f3
luyunfeng <603707288@qq.com>
2019-12-27 18:30:56 +0800

    'fix'

--- app/Http/Auth/IndexController.php
+++ app/Http/Auth/IndexController.php
@@ -45,7 +45,7 @@ class IndexController{
 
         $user = BeanFactory::getBean('UserLogic')->login($data);
         //登陆成功之后的异步处理
-        Task::async('LoginTask','imprint',[$user['id'],['last_login_ip'=>ip(),'last_login_time'=>Carbon::now()->toDateTimeString()]]);
+        Task::async('LoginTask','imprint',[['mobile'=>$user['mobile']],['last_login_ip'=>ip(),'last_login_time'=>Carbon::now()->toDateTimeString()]]);
 
         return $user;
     }
