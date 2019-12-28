9b571b39bd021916f83c519d83541c405f101429
luyunfeng <603707288@qq.com>
2019-12-28 11:31:12 +0800

    'fix'

--- app/Validator/UserValidator.php
+++ app/Validator/UserValidator.php
@@ -6,8 +6,7 @@ namespace App\Validator;
 
 use Swoft\Validator\Annotation\Mapping\Confirm;
 use Swoft\Validator\Annotation\Mapping\IsString;
-use Swoft\Validator\Annotation\Mapping\Max;
-use Swoft\Validator\Annotation\Mapping\Min;
+use Swoft\Validator\Annotation\Mapping\Length;
 use Swoft\Validator\Annotation\Mapping\Mobile;
 use Swoft\Validator\Annotation\Mapping\Validator;
 
@@ -30,8 +29,7 @@ class UserValidator
 
     /**
      * @IsString()
-     * @Max(value=16,message="密码长度不得大于16位")
-     * @Min(value=8,message="密码长度不得小于8位")
+     * @Length(min=8,max=16,message="密码长度需在8~16位之间")
      *
      * @var string
      */
