31e053635d7a76f7fa70fd8cef939a518ff35776
luyunfeng <603707288@qq.com>
2019-12-30 14:06:20 +0800

    'fix'

--- config/regex.php
+++ config/regex.php
@@ -1,5 +1,5 @@
 <?php
 return [
-    'international_phone' => '/^\+\d{2,4}\-1[3-9][0-9]\d{8}$/',
+    'international_phone' => '/^(\+\d{2,4}\-)*1[3-9][0-9]\d{8}$/',
     'amount' => '/^([1-9]\d*|0)(\.(\d){1,2})?$/',
 ];
