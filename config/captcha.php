965cd2123df277965c9c7e234192e74df40bb5e9
luyunfeng <603707288@qq.com>
2019-12-30 19:04:48 +0800

    'fix'

--- config/captcha.php
+++ config/captcha.php
@@ -1,7 +1,11 @@
 <?php
 
 return [
-    'sms.length' => 6,//短信长度
-    'email.length' => 8,//短信长度
+    'sms' => [
+        'length' => 6,//短信验证码长度
+    ],
+    'email' => [
+        'length' => 8,//邮件验证码
+    ],
     'expire' => 15*60,//15分钟
 ];
