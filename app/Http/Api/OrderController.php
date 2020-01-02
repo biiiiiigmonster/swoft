4531414681aa6ca6ea6321ba9a5086e0cdae8ee4
moyou <603707288@qq.com>
2020-01-02 10:07:14 +0800

    fix

---
+++ app/Http/Api/OrderController.php
@@ -0,0 +1,74 @@
+<?php declare(strict_types=1);
+/**
+ * This file is part of Swoft.
+ *
+ * @link https://swoft.org
+ * @document https://swoft.org/docs
+ * @contact group@swoft.org
+ * @license https://github.com/swoft-cloud/swoft/blob/master/LICENSE
+ */
+
+namespace App\Http\Api;
+
+use Swoft\Http\Server\Annotation\Mapping\Controller;
+use Swoft\Http\Server\Annotation\Mapping\RequestMapping;
+use Swoft\Http\Server\Annotation\Mapping\RequestMethod;
+// use Swoft\Http\Message\Response;
+
+/**
+ * Class OrderController
+ *
+ * @Controller(prefix="/Order")
+ * @package App\Http\Api
+ */
+class OrderController{
+    /**
+     * Get data list. access uri path: /Order
+     * @RequestMapping(route="/Order", method=RequestMethod::GET)
+     * @return array
+     */
+    public function index(): array
+    {
+        return ['item0', 'item1'];
+    }
+
+    /**
+     * Get one by ID. access uri path: /Order/{id}
+     * @RequestMapping(route="{id}", method=RequestMethod::GET)
+     * @return array
+     */
+    public function get(): array
+    {
+        return ['item0'];
+    }
+
+    /**
+     * Create a new record. access uri path: /Order
+     * @RequestMapping(route="/Order", method=RequestMethod::POST)
+     * @return array
+     */
+    public function post(): array
+    {
+        return ['id' => 2];
+    }
+
+    /**
+     * Update one by ID. access uri path: /Order/{id}
+     * @RequestMapping(route="{id}", method=RequestMethod::PUT)
+     * @return array
+     */
+    public function put(): array
+    {
+        return ['id' => 1];
+    }
+
+    /**
+     * Delete one by ID. access uri path: /Order/{id}
+     * @RequestMapping(route="{id}", method=RequestMethod::DELETE)
+     * @return array
+     */
+    public function del(): array
+    {
+        return ['id' => 1];
+    }
+}
