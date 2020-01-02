3c90dd0b388f86266eb0836808f0a4c708d74966
luyunfeng <603707288@qq.com>
2020-01-02 18:23:27 +0800

    'fix'

--- app/Http/Api/OrderController.php
+++ app/Http/Api/OrderController.php
@@ -20,6 +20,7 @@ use Swoft\Http\Server\Annotation\Mapping\Middlewares;
 use Swoft\Http\Server\Annotation\Mapping\RequestMapping;
 use Swoft\Http\Server\Annotation\Mapping\RequestMethod;
 use Swoft\Db\Exception\DbException;
+use Swoft\Log\Helper\Log;
 use Swoft\Validator\Annotation\Mapping\Validate;
 use Swoft\Validator\Annotation\Mapping\ValidateType;
 
@@ -51,6 +52,7 @@ class OrderController{
      */
     public function list(Request $request): array
     {
+        Log::error('我出错啦！');
         $param = $request->get();
         /** @var Int */
         $page = $request->get('page',1);
