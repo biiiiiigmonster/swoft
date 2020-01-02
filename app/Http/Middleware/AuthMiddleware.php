dc639f873bc0aa1b1b8a5c4ab3cb2c2b74331794
luyunfeng <603707288@qq.com>
2020-01-02 11:35:44 +0800

    'fix'

--- app/Http/Middleware/AuthMiddleware.php
+++ app/Http/Middleware/AuthMiddleware.php
@@ -46,10 +46,9 @@ class AuthMiddleware implements MiddlewareInterface
 
         // 判断token
         $token = $request->getHeaderLine("authorization");
-        CLog::info(json_encode($token));
         try {
 //            JWT::$leeway = 60;//这个属性表示可以当前请求token的有效时间再延长60s
-            $decoded = JWT::decode(ltrim($token[0],'bearer '), config('secret.jwt', 'CT5'), ['type' => 'HS256']);
+            $decoded = JWT::decode(ltrim($token,'bearer '), config('secret.jwt', 'CT5'), ['type' => 'HS256']);
             //签发者验证
             if (!$this->issDomainVerify($decoded->iss)) {
                 throw new ApiException('[ 验签失败 ] 来源不可靠',401);
