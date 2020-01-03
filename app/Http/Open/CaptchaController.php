81a3e4f71d47733a41c0c671f34f00a71c5b2bfb
luyunfeng <603707288@qq.com>
2020-01-03 11:09:33 +0800

    'fix'

--- app/Http/Open/CaptchaController.php
+++ app/Http/Open/CaptchaController.php
@@ -16,6 +16,8 @@ use Swoft\Http\Message\Request;
 use Swoft\Http\Server\Annotation\Mapping\Controller;
 use Swoft\Http\Server\Annotation\Mapping\RequestMapping;
 use Swoft\Http\Server\Annotation\Mapping\RequestMethod;
+use Swoft\Limiter\Annotation\Mapping\RateLimiter;
+use Swoft\Log\Helper\CLog;
 use Swoft\Log\Helper\Log;
 use Swoft\Redis\Redis;
 use Swoft\Rpc\Client\Annotation\Mapping\Reference;
@@ -48,6 +50,11 @@ class CaptchaController{
      *
      * @RequestMapping(route="sms", method=RequestMethod::POST)
      * @Validate(validator="CreateCaptchaValidator",fields={"type","scene","mobile"})
+     * @RateLimiter(rate=1,key="request.post('mobile')~':'~request.post('scene')")
+     * 发送验证码接口仅仅只是做了静态的参数验证，如果严格来讲还应该有动态验证，比如注册场景下的验证码发送，
+     * 为了避免短信资源被消耗，发送前可以检测一下该账号是否已经注册，如果已经注册了，就阻断提示；
+     * 这个动态验证暂时可以不做，不影响后续业务逻辑，只是资源消耗而已，不过要做的的话得单独写个场景验证器，
+     * 比如某某情况下才不允许做什么，这里怎么去漂亮的实现，还需细品
      *
      * @param Request $request
      * @return array
